d2a054396c495041e0e0c6b9df603d3c65c5afd0
HamdyReda7 <hamdyreda781@gmail.com>
2026-04-20 21:01:34 +0200

    Done task the forgot password

---
+++ src/pages/forgot_password/ForgotPassword.jsx
@@ -0,0 +1,65 @@
+import { Container, Card, Form, Button, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import tsquareLogo from "../../assets/logo-dark.webp"; 
+import "../../pages/forgot_password/forgot.css";  // css
+
+function ForgotPassword() {
+  const { t, i18n } = useTranslation("auth");
+  const isArabic = i18n.language === "ar";
+  return (
+    <div className="forgot-wrapper" dir={isArabic ? "rtl" : "ltr"}>
+      <Container className="d-flex justify-content-center align-items-center h-100">
+        <Card className="forgot-card shadow border-0 p-4">
+          <Card.Body className="text-center p-0">
+            {/* اللوجو */}
+            <Link to="/">
+              <img
+                src={tsquareLogo}
+                alt="T-Square Logo"
+                className="forgot-logo mb-3"
+                title="Back to Home"
+              />
+            </Link>
+
+            {/* العنوان */}
+            <Card.Title className="fw-bold fs-4 mb-4 text-dark forgot-title">
+              {t("forgot_form.reset_password")}
+            </Card.Title>
+
+            <Form>
+              {/* حقل الإيميل */}
+              <Form.Group
+                className={`mb-3 forgot-form-group ${isArabic ? "text-end" : "text-start"}`}
+              >
+                <Form.Label
+                  className="forgot-label d-block "
+                  style={{ textAlign: isArabic ? "right" : "left" }}
+                >
+                  {t("forgot_form.email_label")}
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  required
+                  placeholder={t("forgot_form.email_placeholder")}
+                  className="forgot-input"
+                />
+              </Form.Group>
+
+              {/* زر الدخول */}
+              <Button
+                type="submit"
+                className="forgot-btn btn-lg w-100 fs-6 fw-bold"
+              >
+                {t("forgot_form.send_email")}
+              </Button>
+            </Form>
+
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
+}
+
+export default ForgotPassword;
