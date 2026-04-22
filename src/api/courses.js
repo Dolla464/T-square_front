ad4ecb1d024d6ed29790a258adba127173662f1c
Adel Moneem <dolla464@gmail.com>
2026-04-22 05:53:42 +0200

    my edits

--- src/api/courses.js
+++ src/api/courses.js
@@ -1,9 +1,9 @@
 import axiosInstance from "./axios"; // ملف إعدادات للأكسيوس
 
 export const fetchUserCategories = (params) => {
-  return axiosInstance.get("/student/categories", { params });
+  return axiosInstance.get("api/student/categories", { params });
 };
 
 export const fetchUserCourses = (params) => {
-  return axiosInstance.get("/student/courses", { params });
+  return axiosInstance.get("api/student/courses", { params });
 };
