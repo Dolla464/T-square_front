5a4f9c726c32674de363355edd03bac39fac0534
Adel Abdel Monaem <dolla464@gmail.com>
2026-04-26 19:22:08 +0300

    Update AllContact.jsx

--- src/components/Contact/AllContact.jsx
+++ src/components/Contact/AllContact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
