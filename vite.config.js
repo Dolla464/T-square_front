7ef0dc58c85e64fae4e7ca7e288610e649666941
mohamed salama <dev.mohamed.salama7@gmail.com>
2026-06-09 02:00:45 +0300

    edits

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+
   base: "/",
+
   build: {
-    minify: "esbuild",
     sourcemap: false,
-    // terserOptions: {
-    //   compress: {
-    //     drop_console: mode === "production",
-    //     drop_debugger: true,
-    //   },
-    },
+    minify: "esbuild",
+
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -20,6 +17,7 @@ export default defineConfig(({ mode }) => ({
             if (id.includes("react-bootstrap") || id.includes("bootstrap")) {
               return "bootstrap-vendor";
             }
+
             if (
               id.includes("react-router-dom") ||
               id.includes("react-router") ||
@@ -27,23 +25,29 @@ export default defineConfig(({ mode }) => ({
             ) {
               return "router-vendor";
             }
+
             if (id.includes("i18next") || id.includes("react-i18next")) {
               return "i18n-vendor";
             }
+
             if (id.includes("sweetalert2") || id.includes("react-hot-toast")) {
               return "ui-vendor";
             }
+
             return "vendor";
           }
         },
       },
     },
+
     chunkSizeWarningLimit: 1000,
   },
+
   server: {
     port: 5173,
     strictPort: true,
   },
+
   preview: {
     port: 4173,
     strictPort: true,
